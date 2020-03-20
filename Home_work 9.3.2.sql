a7e4287a41bf9579aa569a750e6a731ff8354726
Sergey Sinitsyn <sinitsa2001@mail.ru>
2020-03-20 09:19:00 +0300

    ДЗ 9

---
+++ Home_work 9.3.2.sql
@@ -0,0 +1,26 @@
+
+
+/*№3. Создайте хранимую функцию hello(), которая будет возвращать приветствие,
+ в зависимости от текущего времени суток.
+ С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро",
+ с 12:00 до 18:00 функция должна возвращать фразу "Добрый день",
+ с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".*/
+
+-- №2 IF ELSE
+DROP PROCEDURE IF EXISTS hello;
+delimiter //
+CREATE PROCEDURE hello()
+BEGIN
+	IF(CURTIME() BETWEEN '06:00:00' AND '12:00:00') THEN
+		SELECT 'Доброе утро';
+	ELSEIF(CURTIME() BETWEEN '12:00:00' AND '18:00:00') THEN
+		SELECT 'Добрый день';
+	ELSEIF(CURTIME() BETWEEN '18:00:00' AND '00:00:00') THEN
+		SELECT 'Добрый вечер';
+	ELSE
+		SELECT 'Доброй ночи';
+	END IF;
+END //
+delimiter ;
+
+CALL hello();
