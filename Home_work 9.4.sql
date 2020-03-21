c12d1d3c61466b803fb707363c1e5788843ba5f6
Sergey Sinitsyn <sinitsa2001@mail.ru>
2020-03-21 09:54:20 +0300

    ДЗ 9

---
+++ Home_work 9.4.sql
@@ -0,0 +1,24 @@
+/*В таблице products есть два текстовых поля: name с названием товара и description с его описанием. 
+-- Допустимо присутствие обоих полей или одно из них. Ситуация, когда оба поля принимают неопределенное
+-- значение NULL неприемлема. Используя триггеры, добейтесь того, чтобы одно из этих полей или оба поля
+-- были заполнены. При попытке присвоить полям NULL-значение необходимо отменить операцию.*/
+
+DROP TRIGGER IF EXISTS nullTrigger;
+delimiter //
+CREATE TRIGGER nullTrigger BEFORE INSERT ON products
+FOR EACH ROW
+BEGIN
+	IF(ISNULL(NEW.name) AND ISNULL(NEW.description)) THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Trigger Warning! NULL in both fields!';
+	END IF;
+END //
+delimiter ;
+
+INSERT INTO products (name, description, price, catalog_id)
+VALUES (NULL, NULL, 5000, 2); 
+
+INSERT INTO products (name, description, price, catalog_id)
+VALUES ("GeForce GTX 1080", NULL, 15000, 12); 
+
+INSERT INTO products (name, description, price, catalog_id)
+VALUES ("GeForce GTX 1080", "Мощная видеокарта", 15000, 12); 
